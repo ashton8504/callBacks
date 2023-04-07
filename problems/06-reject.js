8d5555c4108e005101117100d6235682939d1f3f
Phaedrus <92445005+ashton8504@users.noreply.github.com>
2023-04-07 09:30:52 -0400

    Update 06-reject.js

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -21,11 +21,18 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
 
+let reject = function (arr, cb) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!cb(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
 };
-
-
 
 
 
